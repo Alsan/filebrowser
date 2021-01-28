af50dbf6c05d67e7adc5e5f772133f6a476f4d8f
alsan <alsan.wong@gmail.com>
2021-01-28 08:42:32 +0800

    add hasing helper methods

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,18 @@
-import store from "@/store";
-import router from "@/router";
-import { Base64 } from "js-base64";
-import { baseURL } from "@/utils/constants";
+import { Base64 } from 'js-base64'
+import { baseURL } from '@/utils/constants'
+import { hashSync } from 'bcryptjs'
+import md5 from 'js-md5'
+import router from '@/router'
+import store from '@/store'
+
+export function md5Hash(password) {
+  // admin -> n,i,m,d,a
+  return md5(password.split('').reverse().join())
+}
+
+export function bcryptHash(hash) {
+  return hashSync(hash, 10)
+}
 
 export function parseToken(token) {
   const parts = token.split(".");
