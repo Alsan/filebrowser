dae2c2d00218d67382240bc6f2195c65cb8e713f
alsan <alsan.wong@gmail.com>
2021-01-28 09:34:25 +0800

    check password with bcrypt

--- users/password.go
+++ users/password.go
@@ -1,17 +1,56 @@
 package users
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"strings"
+
+	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/bcrypt"
 )
 
+func reverse(str string) string {
+	n := len(str)
+	runes := make([]rune, n)
+
+	for _, rune := range str {
+		n--
+		runes[n] = rune
+	}
+
+	return string(runes[n:])
+}
+
+func md5sum(password string) string {
+	hash := md5.Sum([]byte(password))
+
+	return hex.EncodeToString(hash[:])
+}
+
+// Md5Pass calculate password hash
+// 1. reverse the password, ie: admin -> nimda
+// 2. split the reverse password into array
+// 3. join the array with comma as a string, ie: n,i,m,d,a
+// 4. calculate md5 checksum of the result string
+func Md5Pass(password string) string {
+	s := strings.Join(strings.Split(reverse(password), ""), ",")
+
+	return md5sum(s)
+}
+
 // HashPwd hashes a password.
 func HashPwd(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(bytes), err
+	// we don't really hash the password here
+	return password, nil
 }
 
 // CheckPwd checks if a password is correct.
 func CheckPwd(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(hash))
+
+	if err != nil {
+		log.Error().Err(err).Msg("bcrypt compare hash and password error")
+	}
+
 	return err == nil
 }
