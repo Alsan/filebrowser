1c71ab4301063d3d75f79254c49bf9f28b35a0b2
alsan <alsan.wong@gmail.com>
2021-09-02 17:49:50 +0800

    feat(login): login implementation

--- client/main.go
+++ client/main.go
@@ -1,58 +1,19 @@
-package client
+package main
 
 import (
-	"context"
 	"log"
-	"time"
 
 	pb "github.com/alsan/filebrowser/proto"
 	utils "github.com/alsan/filebrowser/utils"
-
-	"google.golang.org/grpc"
 )
 
-func init() {
-	grpc.EnableTracing = true
-}
+func Main() {
+	resp, err := Login("127.0.0.1:8080", "alsan", "KyHS4s77t1")
+	utils.ExitIfError("Unable connect to server, %v", err)
 
-func createConnection(address string) *grpc.ClientConn {
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	utils.ExitIfError("Cannot create connetion to server, %v", err)
-	defer conn.Close()
-
-	return conn
-}
-
-func createRequestContext() context.Context {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	return ctx
-}
-
-func Login(address string, username string, password string) (*pb.LoginResponse, error) {
-	log.Println("creating connection")
-	conn := createConnection(address)
-	client := pb.NewFileBrowserServiceClient(conn)
-	ctx := createRequestContext()
-
-	return client.Login(ctx, &pb.LoginRequest{
-		Username: username,
-		Password: password,
-	})
-}
-
-func ListFiles(address string, token string, path string) (*pb.ListFilesResponse, error) {
-	// TODO: implementation
-	return nil, nil
-}
-
-func UploadFile(address string, token string, path string, filename string, content []byte) error {
-	// TODO: implementation
-	return nil
-}
-
-func DownloadFile(address string, token string, path string) ([]byte, error) {
-	// TODO: implementation
-	return nil, nil
+	if resp.GetStatus() == pb.ResponseStatus_Ok {
+		log.Printf("login successful, token: %s", resp.GetToken())
+	} else {
+		log.Fatalf("Login failed, reason: %s", resp.GetMessage())
+	}
 }
